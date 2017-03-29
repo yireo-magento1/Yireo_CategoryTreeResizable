8ce43b1f1ff28e51f65ffeb3d1c08f409c541489
Jisse Reitsma <info@yireo.com>
2017-03-29 22:14:36 +0200

    JS Code compliance

--- source/skin/adminhtml/default/default/categorytreeresizable/categorytreeresizable.js
+++ source/skin/adminhtml/default/default/categorytreeresizable/categorytreeresizable.js
@@ -1,12 +1,15 @@
-jQuery(document).ready(function() {
-    jQuery("#anchor-content .columns").css('background', 'none');
-    jQuery("#anchor-content .main-col").css('border-left', '3px solid #eee');
-    jQuery("#anchor-content .side-col").resizable({
-        resize: function(event, ui) {
-            var leftWidth = ui.size.width;
-            jQuery("#anchor-content .side-col").css('width', leftWidth + 'px');
-            jQuery("#anchor-content .side-col").css('margin-right', '-' + leftWidth + 'px');
-            jQuery("#anchor-content .main-col").css('margin-left', leftWidth + 'px');
-        }
+"use strict";
+(function ($) {
+    $(function () {
+        $("#anchor-content .columns").css('background', 'none');
+        $("#anchor-content .main-col").css('border-left', '3px solid #eee');
+        $("#anchor-content .side-col").resizable({
+            resize: function (event, ui) {
+                var leftWidth = ui.size.width;
+                $("#anchor-content .side-col").css('width', leftWidth + 'px');
+                $("#anchor-content .side-col").css('margin-right', '-' + leftWidth + 'px');
+                $("#anchor-content .main-col").css('margin-left', leftWidth + 'px');
+            }
+        });
     });
-});
+})(jQuery);
